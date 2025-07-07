db4761fd503ccc06f665ce1301ce7c4a3ee79a28
Anshika Gupta <135423750+anshika-75@users.noreply.github.com>
2025-07-07 17:37:26 +0530

    ShortUrl.js

--- Routes/ShortUrl.js
+++ Routes/ShortUrl.js
@@ -18,17 +18,17 @@ router.get('/data',requireAuth, async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-//dynamic id :url ka aysa part joo baar baar jab change hota hai
+
 router.get('/:id',requireAuth, async (req, res) => {
   const {id } = req.params;
 
   try {
     const apiUrl = `https://jsonplaceholder.typicode.com/todos/${id}`;
 
-    //axios 3rd party ke pass jayenge data fetch krne ke liye
+  
     const response = await axios.get(apiUrl);
 
-    const shortUrl= nanoid(7); // generate short ID
+    const shortUrl= nanoid(7); 
 
     res.json({
       shortUrl,
