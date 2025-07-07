b447cabea33bb31eeeeaf579ed4d46ee4f8ce52a
Anshika Gupta <135423750+anshika-75@users.noreply.github.com>
2025-07-07 17:36:42 +0530

    index.js

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const app = express();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const PORT = 3000;
-//use - redirect karta hai 
-//middle ware aur route ko handle karne ke liyee 
+
 
   app.use(express.json());
   app.use(cookieParser());
-// spi mtlb url
+
 app.use('/api' , Routes )
-//api bani
+
 app.get('/All' , (req,res) => {
    res.send('HELLO')});
 
@@ -20,6 +19,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/anshika')
       .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(PORT , () => console.log(`server is running on port ${PORT}`));
-
-//agar isme app.get use karenge toh /api ka kya he kaam
-//GET PUT POST PATH ye sab http req hoti toh ye kaam karenge 
